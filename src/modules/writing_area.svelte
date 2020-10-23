b69ca27e02270923694dded0bf2d28b8c343c1f0
Yazid <secho771@gmail.com>
2020-10-23 22:57:41 +0100

    multiple backgrounds, removed bg soundtrack, simpler buttons, change font size, favicon and page title

--- src/modules/writing_area.svelte
+++ src/modules/writing_area.svelte
@@ -1,21 +1,25 @@
 <script>
-    // Booleans
-    let is_writer_fs = false;
-    let mute = false;
+    let is_fs = false;
 
     let text_area_value = '';
-
     let text_size = 16;
 
-    // Sound
-    let bgm = new Audio('/bgm.mp3');
     let tw = new Audio('/tw.wav');
-
+    let is_tw_mute = false;
     tw.volume = 0.2;
-    bgm.volume = 0.05;
 
-    bgm.currentTime = 0;
-    bgm.play();
+
+
+    function mute_typewriter() {
+        if (!is_tw_mute) {
+            tw.volume = 0;
+            is_tw_mute = true;
+        }
+        else {
+            tw.volume = 0.2;
+            is_tw_mute = false;
+        }
+    }
 
     function on_write_sound() {
         tw.currentTime = 0;
@@ -23,8 +27,6 @@
     }
 
 
-    // Save to file
-    // TRIGGER DOWNLOAD
     function download(content, fileName, contentType) {
         let a = document.createElement("a");
         let file = new Blob([content], { type: contentType });
@@ -33,20 +35,33 @@
         a.click();
     }
 
+    let background_url = '/bg.png';
+    async function change_background() {
+        const req = await fetch('https://picsum.photos/1920/1080');
+        background_url = req.url;
+    }
+
+    function increase_text() {
+        text_size += 2;
+    }
+
+    function decrease_text() {
+        text_size -= 2;
+    }
+
 </script>
-<main>
+<main style="background-image: url({background_url})">
     <div class="container">
         <div class="panel">
-            <button title="Fullscreen" on:click={() => is_writer_fs = !is_writer_fs}><i class="fas fa-expand"></i></button>
-            <button title="Increase Text Size" on:click={() => text_size += 2}><i class="fas fa-text-height"></i></button>
-            <button title="Background" disabled><i class="fas fa-image"></i></button>
-            <button title="Music" disabled><i class="fas fa-headphones-alt"></i></button>
-            <button title="Mute" on:click={() => bgm.pause()}><i class="fas fa-volume-mute"></i></button>
-
-            <!-- <button title="Copy"><i class="fas fa-copy"></i></button> -->
-            <button title="Save to Device" on:click={() => download(text_area_value, `jellyfish - ${new Date().toLocaleString()}.txt`, 'text/plain')}><i class="fas fa-file-export"></i></button>
+            <button title="Toggle fullscreen mode." on:click={() => is_fs = !is_fs}><i class="fas fa-expand"></i></button>
+            <button title="Make text bigger." on:click={increase_text}><i class="fas fa-heading"></i></button>
+            <button title="Make text smaller." on:click={decrease_text}><i class="fas fa-font"></i></button>
+            <button title="Use a random background image." on:click={change_background}><i class="fas fa-image"></i></button>
+            <button title="Toggle typewriter button click sound." on:click={mute_typewriter}><i class="fas fa-volume-mute"></i></button>
+            <button title="Save as text file." on:click={() => download(text_area_value, `jellyfish - ${new Date().toLocaleString()}.txt`, 'text/plain')}><i class="fas fa-file-export"></i></button>
+            <!-- <button title="Change your preferences." ><i class="fas fa-cog"></i></button> -->
         </div>
-        <textarea style='font-size: {text_size}px' bind:value={text_area_value} on:input={on_write_sound} class:iswriterfs={is_writer_fs} autofocus></textarea>
+        <textarea style='font-size: {text_size}px' bind:value={text_area_value} on:input={on_write_sound} class:isfs={is_fs}></textarea>
     </div>
 </main>
 
@@ -57,7 +72,7 @@
         height: 100vh;
         width: 100vw;
         background: rgb(238, 238, 238);
-        background-image: url("/bg.png");
+        /* background-image: url("/bg.png"); */
         background-size: cover;
         display: flex;
         align-items: center;
@@ -65,14 +80,14 @@
     }
 
     textarea {
-        border-radius: 20px;
+        border-radius: 10px;
         border: none;
-        box-shadow: 0px 20px 35px rgba(0, 0, 0, .15);
+        box-shadow: 0px 30px 45px rgba(0, 0, 0, .05);
 
-        padding: 20px;
+        padding: 40px;
         padding-right: 80px;
 
-        background: rgba(255, 255, 255, .8);
+        background: rgba(255, 255, 255, .95);
 
         /* background-image: url('/opq.png'); */
         /* background-repeat: repeat; */
@@ -85,7 +100,7 @@
         min-width: 300px;
 
         height: 80vh;
-        width: 50vw;
+        width: 32vw;
 
         /* max-height: 95vh;
         max-width: 97vw; */
@@ -104,16 +119,16 @@
     }
 
     div.panel button{
-        margin: 5px;
-        padding: 10px;
+        margin: 10px;
+        padding: 5px;
         cursor: pointer;
-        box-shadow: 0px 2px 5px rgba(0, 0, 0, .2);
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, .1);
 
         border:none;
-        border-radius: 50%;
+        border-radius: 5px;
 
-        background: linear-gradient(45deg, #2557c2, #671da0);
-        color: white;
+        /* background: linear-gradient(45deg, #2557c2, #671da0); */
+        /* color: white; */
     }
 
     div.container:hover >  div.panel{
@@ -122,7 +137,7 @@
         padding: 5px;
     }
 
-    .iswriterfs {
+    .isfs {
         /* height: 95vh; */
         /* width: 97vw; */
 
